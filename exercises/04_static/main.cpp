87f27a120cda6703aa0b4924a8444b12abbcead5
yuzhaohui <zhaohui.yu@bianlifeng.com>
2025-01-04 19:11:08 +0800

    test 4

--- exercises/04_static/main.cpp
+++ exercises/04_static/main.cpp
@@ -3,6 +3,7 @@
 // READ: `static` 关键字 <https://zh.cppreference.com/w/cpp/language/storage_duration>
 // THINK: 这个函数的两个 `static` 各自的作用是什么？
 static int func(int param) {
+    //静态的变量，表示只声明一次，所以只有func第一次调用的时候static_才赋值
     static int static_ = param;
     // std::cout << "static_ = " << static_ << std::endl;
     return static_++;
@@ -10,10 +11,10 @@ static int func(int param) {
 
 int main(int argc, char **argv) {
     // TODO: 将下列 `?` 替换为正确的数字
-    ASSERT(func(5) == ?, "static variable value incorrect");
-    ASSERT(func(4) == ?, "static variable value incorrect");
-    ASSERT(func(3) == ?, "static variable value incorrect");
-    ASSERT(func(2) == ?, "static variable value incorrect");
-    ASSERT(func(1) == ?, "static variable value incorrect");
+    ASSERT(func(5) == 5, "static variable value incorrect");
+    ASSERT(func(4) == 6, "static variable value incorrect");
+    ASSERT(func(3) == 7, "static variable value incorrect");
+    ASSERT(func(2) == 8, "static variable value incorrect");
+    ASSERT(func(1) == 9, "static variable value incorrect");
     return 0;
 }
